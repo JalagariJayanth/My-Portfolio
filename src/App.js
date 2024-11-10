a53ade36c52098d00ebd8ad7ea02131e41af21ce
JalagariJayanth <jayanthjalagari@gmail.com>
2024-11-10 09:58:12 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Routes,BrowserRouter} from 'react-router-dom'
+import {Route, Routes,HashRouter} from 'react-router-dom'
 import Main from "./Components/Main";
 import ProjectDetails from './Components/ProjectDetails';
 
@@ -6,12 +6,12 @@ import ProjectDetails from './Components/ProjectDetails';
 
 const App = () => {
   return(
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
     <Routes>
       <Route exact path="/" Component={Main} />
       <Route exact path="/projects/:id" Component={ProjectDetails} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 
 
